90709634051d6805fa9b087c1cd937fa2bcb4ddd
ARONGEEE <146042899+ARONGEEE@users.noreply.github.com>
2025-04-20 00:08:15 +0900

    Create mypage.php

---
+++ 3/Simul_Identi&Auth/mypage.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: beforelogin.php");
+    exit;
+}
+$user = $_SESSION['user'];
+?>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>마이페이지</title>
+  <link rel="stylesheet" href="login.css">
+
+<body>
+    <!-- 상단 메뉴 (가운데 정렬) -->
+    <div style="display: flex; justify-content: center; gap: 10px; padding: 20px 0;">
+    <a href="index.php" class="myform-btn">홈</a>
+    <a href="mypage.php" class="myform-btn">마이페이지</a>
+     </div>
+
+  <div class="myform-area">
+    <div class="form-area">
+      <div class="form-content">
+        <h2>마이페이지</h2>
+        <p>개인 정보 확인</p>
+      </div>
+      <div class="form-input">
+        <p><strong>ID:</strong> <?= $user['id'] ?></p>
+        <p><strong>Score:</strong> <?= $user['score'] ?></p>
+        <p><strong>Location:</strong> <?= $user['address'] ?></p>
+        <p><strong>Register date:</strong> <?= $user['date'] ?></p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
